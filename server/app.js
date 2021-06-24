3f7776afb151f07551d1e3f31a9bc06f105c83ed
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-06-24 15:23:37 +0700

    Setup For Production

--- server/app.js
+++ server/app.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/user", require("./routes/user"));
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(`${__dirname}/public`));
+	app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+}
+
 app.listen(port, () => {
 	console.log(`Up on port ${port}`);
 });
